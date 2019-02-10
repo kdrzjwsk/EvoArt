4ba3a34f7171e74c918cae9d05094da737e36902
kdrzjwsk <konstancja.andrzejewska@gmail.com>
2019-02-10 15:21:36 +0000

    shapes

--- genetics.js
+++ genetics.js
@@ -2,14 +2,30 @@ function draw() {
   var canvas = document.getElementById("replica");
   var ctx = canvas.getContext("2d");
 
-  var size = 50;
+  var size = 200;
+  var minRadius = 5;
+  var maxRadius = 50;
+  for (var i = 0; i < size; i++) {
+    var x = Math.random()*canvas.width;
+    var y = Math.random()*canvas.height;
+    var radius = Math.random()*(maxRadius - minRadius) + minRadius;
+    var sAngle = Math.random()*Math.PI; //or 0*Math.PI
+    var eAngle = (Math.random()+1)*Math.PI; //or 2*Math.PI
+    var counterclockwise = (Math.random() >= 0.5);
+    ctx.beginPath();
+    ctx.arc(x, y, radius, sAngle, eAngle, counterclockwise);
+    ctx.lineWidth = 0.5;
+    ctx.strokeStyle = randomRGB();
+    ctx.stroke();
+  };
+};
 
-  for (int i = 0; i < size; i++) {
-    var x = Math.random();
-    var y = Math.random();
-
-  }
-}
+function randomRGB() {
+  var red = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  return("rgb(" + red + ", " + blue + ", " + green + ")");
+};
 
 //DNA = random rgba + random vertices
 //Population = number of individual DNAs
